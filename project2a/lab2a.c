07345a617112f4344b73582e159d4e8f9fe00e7b
zhehaowang <wangzhehao410305@gmail.com>
2016-04-24 18:17:48 -0700

    project2a: add skeleton code

--- project2a/lab2a.c
+++ project2a/lab2a.c
@@ -3,8 +3,8 @@
 #include <getopt.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
-#define _GNU_SOURCE
 #include <pthread.h>
 
 int num_threads;
@@ -14,30 +14,71 @@ int opt_yield;
 
 long long counter;
 pthread_mutex_t add_mutex;
+int add_spin = 0;
+pthread_t *threads;
 
 void add(long long *pointer, long long value) {
   long long sum = *pointer + value;
   if (opt_yield)
-    // remember to change back to pthread_yield on Linux
-    pthread_yield_np();
+    pthread_yield();
   *pointer = sum;
 }
 
 void* thread_func() {
   int i = 0;
+  long long prev = 0;
+  long long sum = 0;
+
   for (i = 0; i < num_iterations; i++) {
     switch (opt_sync) {
-      case 's':
-        add(&counter, 1);
+      case 'c':
+        do {
+          prev = counter;
+          sum = prev + 1;
+          if (opt_yield) {
+            pthread_yield();
+          }
+        } while (__sync_val_compare_and_swap(&counter, prev, sum) != prev);
         break;
       case 'm':
         pthread_mutex_lock(&add_mutex);
         add(&counter, 1);
         pthread_mutex_unlock(&add_mutex);
         break;
-      case 'c':
+      case 's':
+        while (__sync_lock_test_and_set(&add_spin, 1));
+        add(&counter, 1);
+        __sync_lock_release(&add_spin);
         break;
       default:
+        add(&counter, 1);
+        break;
+    }
+  }
+
+  for (i = 0; i < num_iterations; i++) {
+    switch (opt_sync) {
+      case 'c':
+        do {
+          prev = counter;
+          sum = prev - 1;
+          if (opt_yield) {
+            pthread_yield();
+          }
+        } while (__sync_val_compare_and_swap(&counter, prev, sum) != prev);
+        break;
+      case 'm':
+        pthread_mutex_lock(&add_mutex);
+        add(&counter, -1);
+        pthread_mutex_unlock(&add_mutex);
+        break;
+      case 's':
+        while (__sync_lock_test_and_set(&add_spin, 1));
+        add(&counter, -1);
+        __sync_lock_release(&add_spin);
+        break;
+      default:
+        add(&counter, -1);
         break;  
     }
   }
@@ -77,18 +118,39 @@ int main(int argc, char **argv)
   printf("sync method: %c\n", opt_sync);
 
   int i = 0;
-  for (i = 0; i < num_threads; i++) {
+  int ret = 0;
+  threads = (pthread_t *)malloc(sizeof(pthread_t) * num_threads);
 
-  }
+  struct timespec start_time;
+  clock_gettime(CLOCK_MONOTONIC, &start_time);
 
   if (opt_sync == 'm') {
     pthread_mutex_init(&add_mutex, NULL);
   }
 
   for (i = 0; i < num_threads; i++) {
+    ret = pthread_create(&(threads[i]), NULL, thread_func, NULL);
+    if (ret < 0) {
 
+    }
   }
 
+  for (i = 0; i < num_threads; i++) {
+    ret = pthread_join(threads[i], NULL);
+    if (ret < 0) {
 
+    }
+  }
+
+  struct timespec end_time;
+  clock_gettime(CLOCK_MONOTONIC, &end_time);
+  
+  long long elasped_time_ns = (end_time.tv_sec - start_time.tv_sec) * 1000000000;
+  elasped_time_ns += end_time.tv_nsec;
+  elasped_time_ns -= start_time.tv_nsec;
+  
+  printf("Took %lld nanoseconds\n", elasped_time_ns);
+
+  free(threads);
   return 0;
 }
