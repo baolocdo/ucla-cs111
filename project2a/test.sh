dafe5691031271b2a74d1b8f844ca0c47fb3b576
zhehaowang <wangzhehao410305@gmail.com>
2016-04-24 19:01:25 -0700

    project2a: test script skeleton

--- project2a/test.sh
+++ project2a/test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo "./lab2a --iterations=10 --threads=1"
-for i in `seq 1 10`; do
-	./lab2a --iterations=10 --threads=1 2>stderr.txt 1>stdout.txt
+rm -rf stderr.txt
+rm -rf stdout.txt
+
+threads=(1 2 4 8 12 16 20 24 28 32)
+iterations=(1 10 100 200 500 1000 2000 5000 10000 100000)
+
+for it in "${threads[@]}"; do
+	for ii in "${iterations[@]}"; do
+		echo "./lab2a --iterations=$ii --threads=$it"
+		echo "./lab2a --iterations=$ii --threads=$it" >> stdout.txt
+		echo "./lab2a --iterations=$ii --threads=$it" >> stderr.txt
+		for i in `seq 1 10`; do
+			./lab2a --iterations=$ii --threads=$it 2>>stderr.txt 1>>stdout.txt
+		done
+	done
 done
